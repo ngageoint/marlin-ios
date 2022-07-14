43d407adbf515e7ddca1d84336ca183c8997de65
Dan Barela <daniel.barela@gmail.com>
2022-07-14 08:00:01 -0600

    cleanup

--- Marlin/Marlin/Map/Mixins/PersistedMapState.swift
+++ Marlin/Marlin/Map/Mixins/PersistedMapState.swift
@@ -11,8 +11,6 @@ import MapKit
 class PersistedMapState: NSObject, MapMixin {
     var mapView: MKMapView?
     
-//    @AppStorage("mapRegion") var mapRegion: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), latitudinalMeters: 20000, longitudinalMeters: 20000)
-    
     func setupMixin(mapView: MKMapView, marlinMap: MarlinMap, scheme: MarlinScheme?) {
         self.mapView = mapView
         let region = UserDefaults.standard.mapRegion
