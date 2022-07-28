dbdd3392abfe755f4c3dc3c43e45f25d736a68b5
Dan Barela <daniel.barela@gmail.com>
2022-07-28 14:44:46 -0600

    app now remembers what tab you were on

--- Marlin/Marlin/MarlinTabView.swift
+++ Marlin/Marlin/MarlinTabView.swift
@@ -19,7 +19,7 @@ struct MarlinTabView: View {
     
     @StateObject var itemWrapper: ItemWrapper = ItemWrapper()
     @State var selection: String? = nil
-    @State private var selectedTab = "map"
+    @AppStorage("selectedTab") var selectedTab: String = "map"
     @State var menuOpen: Bool = false
     
     @StateObject var dataSourceList: DataSourceList = DataSourceList()
@@ -33,127 +33,127 @@ struct MarlinTabView: View {
     var marlinMap = MarlinMap()
     
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                TabView(selection: $selectedTab) {
-                    NavigationView {
-                        VStack {
-                            ZStack(alignment: .topLeading) {
-                                marlinMap
-                                    .mixin(AsamMap())
-                                    .mixin(ModuMap())
-                                    .mixin(LightMap())
-                                    .mixin(BottomSheetMixin())
-                                    .mixin(PersistedMapState())
-                                    .navigationTitle("Marlin")
-                                    .navigationBarTitleDisplayMode(.inline)
-                                    .navigationBarBackButtonHidden(true)
-                                    .modifier(Hamburger(menuOpen: $menuOpen))
-                                VStack {
-                                    // top of map
-                                    HStack(alignment: .top, spacing: 0) {
-                                        Spacer()
-                                        // top right button stack
-                                        VStack(alignment: .trailing, spacing: 16) {
-                                            NavigationLink {
-                                                MapSettings()
-                                            } label: {
-                                                MaterialFloatingButton(imageName: .constant("square.3.stack.3d"))
-                                            }
-                                            .isDetailLink(false)
-                                            .offset(x: -8, y: 16)
-                                            .fixedSize()
-                                        }
-                                    }
+        ZStack {
+            TabView(selection: $selectedTab) {
+                NavigationView {
+                    VStack {
+                        ZStack(alignment: .topLeading) {
+                            marlinMap
+                                .mixin(AsamMap())
+                                .mixin(ModuMap())
+                                .mixin(LightMap())
+                                .mixin(BottomSheetMixin())
+                                .mixin(PersistedMapState())
+                                .navigationTitle("Marlin")
+                                .navigationBarTitleDisplayMode(.inline)
+                                .navigationBarBackButtonHidden(true)
+                                .modifier(Hamburger(menuOpen: $menuOpen))
+                            VStack {
+                                // top of map
+                                HStack(alignment: .top, spacing: 0) {
                                     Spacer()
-                                    // bottom of map
-                                    HStack(alignment: .bottom, spacing: 0) {
-                                        Spacer()
-                                        // bottom right button stack
-                                        VStack(alignment: .trailing, spacing: 16) {
-                                            UserTrackingButton(mapView: marlinMap.mutatingWrapper.mapView)
-                                                .offset(x: -8, y: -24)
-                                                .fixedSize()
+                                    // top right button stack
+                                    VStack(alignment: .trailing, spacing: 16) {
+                                        NavigationLink {
+                                            MapSettings()
+                                        } label: {
+                                            MaterialFloatingButton(imageName: .constant("square.3.stack.3d"))
                                         }
+                                        .isDetailLink(false)
+                                        .offset(x: -8, y: 16)
+                                        .fixedSize()
+                                    }
+                                }
+                                Spacer()
+                                // bottom of map
+                                HStack(alignment: .bottom, spacing: 0) {
+                                    Spacer()
+                                    // bottom right button stack
+                                    VStack(alignment: .trailing, spacing: 16) {
+                                        UserTrackingButton(mapView: marlinMap.mutatingWrapper.mapView)
+                                            .offset(x: -8, y: -24)
+                                            .fixedSize()
                                     }
                                 }
                             }
-                            NavigationLink(tag: "detail", selection: $selection) {
-                                DataDetailView(data: itemWrapper.dataSource)
+                        }
+                        NavigationLink(tag: "detail", selection: $selection) {
+                            DataDetailView(data: itemWrapper.dataSource)
+                        } label: {
+                            EmptyView()
+                        }
+                        .isDetailLink(false)
+                        .hidden()
+                        
+                        ForEach(dataSourceList.nonTabs) { dataSource in
+                            
+                            NavigationLink(tag: "\(dataSource.key)List", selection: $selection) {
+                                createListView(dataSource: dataSource)
                             } label: {
                                 EmptyView()
                             }
+                            
                             .isDetailLink(false)
                             .hidden()
-                            
-                            ForEach(dataSourceList.nonTabs) { dataSource in
-                                
-                                NavigationLink(tag: "\(dataSource.key)List", selection: $selection) {
-                                    createListView(dataSource: dataSource)
-                                } label: {
-                                    EmptyView()
-                                }
-                                
-                                .isDetailLink(false)
-                                .hidden()
-                            }
-                        }
-                        .onReceive(self.appState.$popToRoot) { popToRoot in
-                            if popToRoot {
-                                self.selection = "map"
-                                self.appState.popToRoot = false
-                            }
                         }
                     }
-                    .tag("map")
-                    .tabItem {
-                        Label("Map", systemImage: "map.fill")
-                    }
-                    // this affects text buttons, image buttons need .foregroundColor set on them
-                    .tint(Color(scheme.containerScheme.colorScheme.onPrimaryColor))
-                    .navigationViewStyle(.stack)
-                    .statusBar(hidden: false)
-                    
-                    ForEach(dataSourceList.tabs, id: \.self) { dataSource in
-                        NavigationView {
-                            createListView(dataSource: dataSource)
-                                .modifier(Hamburger(menuOpen: $menuOpen))
+                    .onReceive(self.appState.$popToRoot) { popToRoot in
+                        if popToRoot {
+                            self.selection = "map"
+                            self.appState.popToRoot = false
                         }
-                        .tabItem {
-                            if let imageName = dataSource.dataSource.imageName {
-                                Label(dataSource.dataSource.dataSourceName, image: imageName)
-                            } else if let imageName = dataSource.dataSource.systemImageName {
-                                Label(dataSource.dataSource.dataSourceName, systemImage: imageName)
-                            } else {
-                                Label(dataSource.dataSource.dataSourceName, systemImage: "list.bullet.rectangle.fill")
-                            }
-                        }
-                        .tag("\(dataSource.key)List")
                     }
                 }
-                .onReceive(viewDataSourcePub) { output in
-                    if let dataSource = output.object as? DataSource {
-                        viewData(dataSource)
-                    }
+                .tag("map")
+                .tabItem {
+                    Label("Map", systemImage: "map.fill")
                 }
-                .onReceive(mapFocus) { output in
-                    selectedTab = "map"
-                    self.appState.popToRoot = true
-                }
-                .onReceive(switchTabPub) { output in
-                    if let output = output as? String {
-                        if dataSourceList.tabs.contains(where: { item in
-                            item.key == output
-                        }) {
-                            selectedTab = "\(output)List"
-                        } else {
-                            selectedTab = "map"
-                            selection = "\(output)List"
-                        }
-                        self.menuOpen = false
-                    }
-                }
+                // this affects text buttons, image buttons need .foregroundColor set on them
+                .tint(Color(scheme.containerScheme.colorScheme.onPrimaryColor))
+                .navigationViewStyle(.stack)
+                .statusBar(hidden: false)
                 
+                ForEach(dataSourceList.tabs, id: \.self) { dataSource in
+                    NavigationView {
+                        createListView(dataSource: dataSource)
+                            .modifier(Hamburger(menuOpen: $menuOpen))
+                    }
+                    .tabItem {
+                        if let imageName = dataSource.dataSource.imageName {
+                            Label(dataSource.dataSource.dataSourceName, image: imageName)
+                        } else if let imageName = dataSource.dataSource.systemImageName {
+                            Label(dataSource.dataSource.dataSourceName, systemImage: imageName)
+                        } else {
+                            Label(dataSource.dataSource.dataSourceName, systemImage: "list.bullet.rectangle.fill")
+                        }
+                    }
+                    .tag("\(dataSource.key)List")
+                }
+            }
+            .onReceive(viewDataSourcePub) { output in
+                if let dataSource = output.object as? DataSource {
+                    viewData(dataSource)
+                }
+            }
+            .onReceive(mapFocus) { output in
+                selectedTab = "map"
+                self.appState.popToRoot = true
+            }
+            .onReceive(switchTabPub) { output in
+                if let output = output as? String {
+                    if dataSourceList.tabs.contains(where: { item in
+                        item.key == output
+                    }) {
+                        selectedTab = "\(output)List"
+                    } else {
+                        selectedTab = "map"
+                        selection = "\(output)List"
+                    }
+                    self.menuOpen = false
+                }
+            }
+                
+            GeometryReader { geometry in
                 SideMenu(width: geometry.size.width - 56,
                      isOpen: self.menuOpen,
                      menuClose: self.openMenu,
