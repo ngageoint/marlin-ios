9806256fdeca4a21a4f4453019042e808079ebb2
Dan Barela <daniel.barela@gmail.com>
2022-08-25 06:08:24 -0600

    build number script

---
+++ Marlin/Scripts/set_build_number.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+#  set_build_number.sh
+#  Marlin
+#
+
+BRANCH=${1:-'master'}
+APP_BUILD_NUMBER=$(expr $(git rev-list $BRANCH --count) - $(git rev-list HEAD..$BRANCH --count))
+
+BUILD_NUMBER=${APP_BUILD_NUMBER}
+echo "Updating build number to $BUILD_NUMBER using branch '$BRANCH'."
+
+APP_INFO_PLIST="${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+DSYM_INFO_PLIST="${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$APP_INFO_PLIST"
+if [ -f "$DSYM_INFO_PLIST" ] ; then
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$DSYM_INFO_PLIST"
+fi
