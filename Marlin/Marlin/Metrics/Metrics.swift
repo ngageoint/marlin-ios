884d2f287b9cfb578d99d588e57147625e177308
Dan Barela <daniel.barela@gmail.com>
2022-11-01 09:22:46 -0600

    metrics tracking class

--- Marlin/Marlin/Metrics/Metrics.swift
+++ Marlin/Marlin/Metrics/Metrics.swift
@@ -10,8 +10,8 @@ import MatomoTracker
 
 class Metrics {
     
-    static let MATOMO_URL = "https://example.com/piwik.php"
-    static let MATOMO_SITEID = "1"
+    static let MATOMO_URL = "https://webanalytics.nga.mil/matomo.php"
+    static let MATOMO_SITEID = "795"
     
     static let shared = Metrics()
     
@@ -21,50 +21,64 @@ class Metrics {
     
     func appLaunch() {
         NSLog("Record App Launch")
-//        MatomoTracker.shared.track(view:["app"])
+        MatomoTracker.shared.track(view:["app"])
     }
     
     func mapView() {
         NSLog("Record Map View")
-//        MatomoTracker.shared.track(view:["app", "map"])
+        MatomoTracker.shared.track(view:["app", "map"])
     }
     
     func mapSettingsView() {
         NSLog("Record Map Settings View")
-//        MatomoTracker.shared.track(view:["app", "map", "settings"])
+        MatomoTracker.shared.track(view:["app", "map", "settings"])
     }
     
     func sideNavigationView() {
-        NSLog("Record Side Navigation VIew")
-//        MatomoTracker.shared.track(view:["app", "sideNavigation"])
+        NSLog("Record Side Navigation View")
+        MatomoTracker.shared.track(view:["app", "sideNavigation"])
     }
     
     func settingsView() {
         NSLog("Record Settings View")
-//        MatomoTracker.shared.track(view:["app", "settings"])
+        MatomoTracker.shared.track(view:["app", "settings"])
     }
     
     func submitReportView() {
         NSLog("Record Submit Report View")
-        //        MatomoTracker.shared.track(view:["app", "submitReport"])
+        MatomoTracker.shared.track(view:["app", "submitReport"])
     }
     
     func searchView() {
         NSLog("Record Search View")
-//        MatomoTracker.shared.track(view:["app", "map", "search"])
+        MatomoTracker.shared.track(view:["app", "map", "search"])
     }
     
     func dataSourceList(dataSource: any DataSource.Type) {
         NSLog("Record Data Source List \(dataSource.key)")
-//        MatomoTracker.shared.track(view:["app", "\(dataSource.key)List"])
+        MatomoTracker.shared.track(view:["app", "\(dataSource.key)List"])
     }
     
     func dataSourceDetail(dataSource: any DataSource.Type) {
         NSLog("Record Data Source Detail \(dataSource.key)")
-//        MatomoTracker.shared.track(view:["app", "\(dataSource.key)List", "\(dataSource.key)Detail"])
+        MatomoTracker.shared.track(view:["app", "\(dataSource.key)List", "\(dataSource.key)Detail"])
     }
+    
+    func search(query: String, resultCount: Int) {
+        NSLog("Record search")
+        MatomoTracker.shared.trackSearch(query: query, category: nil, resultCount: resultCount)
+    }
+    
+    func fileDownload(url: URL?) {
+        guard let url else {
+            return
+        }
+        let event = Event(tracker: MatomoTracker.shared, action: ["download"], url: url, eventCategory: "download", eventAction: "download", eventName: nil, eventValue: nil, customTrackingParameters: ["download": url.absoluteString], dimensions: [], isCustomAction: true)
+        MatomoTracker.shared.track(event)
+    }
+    
 }
 
-//extension MatomoTracker {
-//    static let shared: MatomoTracker = MatomoTracker(siteId: Metrics.MATOMO_SITEID, baseURL: URL(string: Metrics.MATOMO_URL)!)
-//}
+extension MatomoTracker {
+    static let shared: MatomoTracker = MatomoTracker(siteId: Metrics.MATOMO_SITEID, baseURL: URL(string: Metrics.MATOMO_URL)!)
+}
