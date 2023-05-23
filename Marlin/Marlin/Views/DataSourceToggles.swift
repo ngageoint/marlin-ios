1bcc3a9dcff38ba319cc112261ec4d03b909b488
Dan Barela <daniel.barela@gmail.com>
2023-05-23 13:26:07 -0600

    data source toggles properly reflect current state

--- Marlin/Marlin/Views/DataSourceToggles.swift
+++ Marlin/Marlin/Views/DataSourceToggles.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct DataSourceToggles: View {
     @EnvironmentObject var dataSourceList: DataSourceList
+    // force an updates
+    @State var date = Date()
+    
+    let mappedDataSourcesUpdatedPub = NotificationCenter.default.publisher(for: .MappedDataSourcesUpdated)
     
     var body: some View {
         ForEach(dataSourceList.allTabs, id: \.self) { dataSource in
@@ -28,6 +32,9 @@ struct DataSourceToggles: View {
                 .accessibilityElement(children: .contain)
                 .accessibilityLabel("\(dataSource.dataSource.key) Map Toggle")
             }
+        }
+        .onReceive(mappedDataSourcesUpdatedPub) { updated in
+            date = Date()
         }
     }
 }
