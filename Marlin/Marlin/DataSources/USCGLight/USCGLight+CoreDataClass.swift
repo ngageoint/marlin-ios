472dfb8731d195073d2e01b31345846c35844fe9
Dan Barela <daniel.barela@gmail.com>
2023-08-11 13:32:41 -0600

    core data model for USCG Lights

---
+++ Marlin/Marlin/DataSources/USCGLight/USCGLight+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  USCGLight+CoreDataClass.swift
+//  Marlin
+//
+//  Created by Daniel Barela on 8/11/23.
+//
+
+import Foundation
+import CoreData
+
+class USCGLight: NSManagedObject {
+    
+}
